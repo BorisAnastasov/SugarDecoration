a9609ef01c78c9add4c038f11ac956b5538e8f85
BorisAnastasov <boris_ywu@abv.bg>
2024-03-06 22:51:04 +0200

    fixed the Core layer

--- SugarDecoration.Core/Models/IProductServiceModel.cs
+++ SugarDecoration.Core/Models/IProductServiceModel.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SugarDecoration.Core.Contracts
+namespace SugarDecoration.Core.Models
 {
-	internal interface IProductService
+	internal interface IProductServiceModel
 	{
-		 Task <Cak>
+
 	}
 }
