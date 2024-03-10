15f486c573bc40c7acf529ad02c1cdfafdd925d1
BorisAnastasov <boris_ywu@abv.bg>
2024-03-10 18:26:40 +0200

    Made some services

---
+++ SugarDecoration.Core/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SugarDecoration.Core.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
