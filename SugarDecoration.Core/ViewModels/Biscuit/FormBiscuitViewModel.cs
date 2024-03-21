982cd4c10e699ccba130b5866659070a209b32c8
BorisAnastasov <boris_ywu@abv.bg>
2024-03-21 23:24:13 +0200

    commit

--- SugarDecoration.Core/ViewModels/Biscuit/FormBiscuitViewModel.cs
+++ SugarDecoration.Core/ViewModels/Biscuit/FormBiscuitViewModel.cs
@@ -9,7 +9,7 @@ namespace SugarDecoration.Core.ViewModels.Biscuit
 		public string Price { get; set; } = string.Empty;
 		public int Quantity { get; set; }
 		public int CategoryId { get; set; }
-		public string ImageUrl { get; set; } = string.Empty;
+		public string ImageUrl { get; set; }
 		public DateTime CreatedOn { get; set; }
         public IEnumerable<CakeCategoryViewModel> CakeCategories { get; set; }
 					= new List<CakeCategoryViewModel>();
